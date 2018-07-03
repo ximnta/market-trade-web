97920d22985ef5737f19f793e2f0cc8b067c99c6
Ximena Tena <ximnta@gmail.com>
2018-07-03 07:58:22 +0100

    Update table pagination

--- src/app/table/table.js
+++ src/app/table/table.js
@@ -1,15 +1,21 @@
 angular.module('market-trade.table', ['market-trade.service'])
     .component('tradeTable', {
         templateUrl: 'table/table.html',
-        controller: ['tradeService', function (tradeService) {
+        controller: ['$filter', 'tradeService', function ($filter, tradeService) {
             
             var ctrl = this;
             tradeService.getMessages().then(function (m) {
-                ctrl.messages = m;
+                ctrl.originalMessages = m;
+                ctrl.filterMessages();
             });
 
             this.titles = ['User id', 'Currency From', 'Currency To', 'Amount Sell', 'Amount Buy', 'Rate', 'Time Placed', 'Originating Country' ];
-            this.page = 1;
             this.pageSize = 15;
+
+            this.filterMessages = function (){
+                ctrl.page = 1;
+                ctrl.messages = $filter('filter')(ctrl.originalMessages, {originatingCountry: ctrl.country, currencyFrom: ctrl.from, currencyTo: ctrl.to});
+;               ctrl.totalPages = Math.ceil(ctrl.messages.length / ctrl.pageSize) || 1;
+            }
         }]
     });
